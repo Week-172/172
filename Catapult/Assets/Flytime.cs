e5168b0006ea7af74191d5a986fa19a20fe44d1c
atomicrogue <60835026+atomicrogue@users.noreply.github.com>
2020-11-02 14:47:00 +0530

    I made it endless

--- Catapult/Assets/Flytime.cs
+++ Catapult/Assets/Flytime.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class Flytime : MonoBehaviour
 {
-    Animator anim;
+    GameObject Pilot;
+    Animator anima;
     Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim=GetComponent<Animator>();
+        rb2d=GetComponent<Rigidbody2D>();
+        anima=GetComponent<Animator>();
     }
-    void OnTriggerExit(Collision col)
+    void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.CompareTag("FlyTrigger"))
         {
-            anim.Play("BoiFlies");
+            anima.SetBool("EnterFlyZone",true);
             rb2d.bodyType=RigidbodyType2D.Kinematic;
         }
+        if(anima.GetBool("EnterFlyZone"))
+        {
+            anima.Play("BoiFlies");
+            rb2d.freezeRotation=true;
+            rb2d.velocity=new Vector2(rb2d.velocity.x,0);
+            rb2d.isKinematic=true;
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
+        }
+
     }
 
     // Update is called once per frame
---
+++ Catapult/Assets/MainPlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayerScript : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d=GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb2d.bodyType==RigidbodyType2D.Kinematic)
+        {
+            if(Input.GetKey("up"))
+            rb2d.velocity=new Vector2(rb2d.velocity.x, 20f);
+            else if(Input.GetKey("down"))
+            rb2d.velocity=new Vector2(rb2d.velocity.x, -20f);
+            else
+            rb2d.velocity=new Vector2(rb2d.velocity.x, 0f);
+
+        }
+    }
+}
