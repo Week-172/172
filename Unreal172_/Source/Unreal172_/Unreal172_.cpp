925a821eaf3b461502f4b81d05fb7c8bcecd42c0
atomicrogue <60835026+atomicrogue@users.noreply.github.com>
2020-11-24 18:50:49 +0530

    Deleted the Unreal Project lol. Due to an error from my side

--- Unreal172_/Source/Unreal172_/Unreal172_.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Unreal172_.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal172_, "Unreal172_" );
